ee8bc754dcdfb98bec94b3415a00c8d69ee9ea1c
huiyahuny <82598289+huiyahuny@users.noreply.github.com>
2023-02-11 15:05:54 +0900

    0211

--- app.py
+++ app.py
@@ -72,11 +72,6 @@ def similarity_image():
     print(anch)
     return render_template('1st_test.html', h_path=h_path)
 
-# @app.route("/sim_test", methods=["POST"])
-# def sim_test():
-#     p_path = str(request.form['p_path'])
-#     sim = float(request.form['sim'])
-#     return render_template('sim_test.html', p_path=p_path, sim=sim)
 
 @app.route("/image_similarity", methods=["POST"])
 def image_similarity():
@@ -149,19 +144,18 @@ def image_similarity():
 def stroop():
     return render_template('2nd_test.html')
 
+OX = []
+
 @app.route("/save",methods=['POST']) #flask 웹 페이지 경로
 def save(): # 경로에서 실행될 기능 선언
     correct = []
     my_correct=[]
-    OX = []
+    global OX
     result = request.form['result']
     correct.append(result[0:2])  # 문자열 슬라이싱해서 들고와야한다. 
     my_correct.append(result[3:5])
     OX.append(result[6:8])
-    print(correct)
-    print(my_correct)
-    print(OX)
-    
+
     # # 확인용
     # check = request.form['check']
     # print(check)
@@ -203,6 +197,8 @@ def save(): # 경로에서 실행될 기능 선언
 def text_to_img():
     return render_template('3rd_test.html')
 
+yolo_model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'best.pt', force_reload =True)
+
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
     print('1111111111111111')
@@ -227,15 +223,15 @@ def predict():
     
     
     # Model(YOLOv5 종속 항목 설치)
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'best.pt', force_reload =True)
+
     # Image
     print('asdadasdasdasdas')
     img = PIL.Image.open('image.png')
     print('ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㅁㄴㅇㅁㄴㅇㅁㄴㅇ')
     ########## 이 사진을 어떻게 가지고 올지에 대해서 알아봐야한다. !!
-
+    global yolo_model
     # 추론
-    results = model(img)
+    results = yolo_model(img)
 
 
     # 결과
@@ -294,17 +290,17 @@ def find_diff():
 count = 0
 test_class = ['나비','지렁이','컴퓨터']
 # 밑에 next를 안 넣고 count>=3 을 하면 오류남 (인덱스 에러)/ 왜인진 모르겠지만 맨 마지막 인덱스는 안 나온다.-># 해결 
-OX =[] # db 저장용 
+wrong_ox = list() #db 저장용 
 # html 렌더링
 @app.route('/wrong_img',  methods=['POST','GET'])
 def wrong_img():
     # OX list에 결과값 저장
-    global OX
+    global wrong_ox
     if request.method == 'POST':
         image = str(request.form['button'])
         if 'X' in image:
-            OX.append('정답')
-        else: OX.append('오답')
+            wrong_ox.append('정답')
+        else: wrong_ox.append('오답')
     # 이미지 불러오기
     global count
     global test_class
@@ -369,9 +365,9 @@ def end():
     
     # db 에 정보 저장
     game = '틀린그림찾기'
-    OX1 = OX[0]
-    OX2 = OX[1]
-    OX3 = OX[2]
+    OX1 = wrong_ox[0]
+    OX2 = wrong_ox[1]
+    OX3 = wrong_ox[2]
     
     cursor.execute("""
         INSERT INTO wrong_test (game, OX1,OX2,OX3) VALUES (?,?,?,?)          
@@ -438,25 +434,8 @@ def get_screenshot():
 
 ################### 6번째 게임 : STT ###################
 
-DATABASE_URI = 'ijm.db'
 
-
-
-    # ---- DB에서 구조 생성 ----
-# conn = sql.connect(DATABASE_URI, isolation_level=None)
-# cur = conn.cursor()
-# cur.execute(
-#     'CREATE TABLE IF NOT EXISTS STT (id TEXT, target TEXT, user_sound TEXT, ck TEXT, score TEXT)')
-# id = 1
-# target = "강아지가 방에 들어오면 고양이는 의자 밑에 숨는다"
-
-# conn = sql.connect(DATABASE_URI, isolation_level=None)
-# cur = conn.cursor()
-# cur.execute("""INSERT INTO STT(id, target) 
-#             VALUES(?, ?)""", (id, target))
-
-
-# ----------------------------------------------------------------------연결
+DATABASE_URI = 'sttdb.db'
 
 conn = sql.connect(DATABASE_URI, isolation_level=None)
 cur = conn.cursor()
@@ -480,26 +459,47 @@ def sound():
 
 @app.route('/STT', methods=['POST', 'GET'])
 def STT():
-
+    print('1111111111111111111111')
     String_sound = ''  # 녹음파일 Text
+    print('22222222222222222222')
     String_target = '' # 정답 Text
-
+    print('333333333333333333')
     sleep(5)
-
+    count = 1
+    print('4444444444444444')
     
     #---------------------------------------------------------------------------
     #      STT Open API
     #---------------------------------------------------------------------------
-
+    print('여기까지는 되는거?')
     if request.method == 'POST':
         openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Recognition"
+        print('5555555')
         accessKey = "f0f9fd15-daef-4655-b516-d7a9711c696a" 
+        print('6666666')
         audioFilePath = request.files['recode'] # 다운로드한 음성파일을 여기에 넣어서 Text로 바꾸기
-        languageCode = "korean"
 
+        print('777777777')
+        # audioFilePath.save('녹음파일.wav')
+        print('888888888')
+        languageCode = "korean"
+        print('999999999999')
+        #file = open('nefile', "rb")
+        #audioContents= wavfile.read("녹음파일.wav") ## !!
+        #print(audioContents)
+        #audio_binary = tf.io.read_file(audioFilePath)
+        # audioFile = request.files['recode']
         data = audioFilePath.read()
         audioContents = base64.b64encode(data).decode("utf8")
+        # inMemoryFile = BytesIO(audioContents)
 
+
+        print('ㄱㄱㄱㄱㄱㄱㄱㄱㄱㄱ')
+        #audioContents = base64.b64encode(file.read()).decode("utf8")
+        # audioContents = base64.b64encode(inMemoryFile.getvalue()).decode("utf8")
+        print('ㄴㄴㄴㄴㄴㄴ')
+        #file.close()
+        
         requestJson = {    
             "argument": {
                 "language_code": languageCode,
@@ -528,12 +528,16 @@ def STT():
         # 녹음한 음성을 처리한 결과를 List변수에 담는다.
         
         
+        # dic = {'1' : "안녕하세요. 오늘도 멋진 하루 되세요"}
+        
+        
         # NLP 유사도검사를 위해 정답Text와 녹음하고 Text로 바꾼 결과를 변수에 담에서 NLP모델에 넘긴다.
         # 녹음파일 Text
         String_sound = List
         
         # 정답Text
         String_target = sound_target
+        
         
         
         #---------------------------------------------------------------------------
@@ -584,16 +588,10 @@ def STT():
         else:
             String += 'X'
             
-            
-
         # os.remove(audioFilePath)
-        #--------------------------------------------------------------------------
-        # DB에 데이터 저장
-        #--------------------------------------------------------------------------
-        
-        # print(sentence2)
-        # print(sentence1)
-        # print(String)
+        print(sentence2)
+        print(sentence1)
+        print(String)
         
         conn = sql.connect(DATABASE_URI, isolation_level=None)
         cur = conn.cursor()
@@ -605,7 +603,7 @@ def STT():
         conn.commit()
         cur.close()
         
-        # -------------------------------------------------------------------------
+        #-----------------------------------------------------------------------------
         
         conn = sql.connect(DATABASE_URI, isolation_level=None)
         cur = conn.cursor()
@@ -620,169 +618,11 @@ def STT():
         stt_user_sound = STT_Data[5]
         stt_ck = STT_Data[7]
         stt_score = STT_Data[9]
-
-        # print(stt_id)
-        # print(stt_target)
-        # print(stt_user_sound)
-        # print(stt_ck)
-        # print(stt_score)
         
-    #                                               정답문장          TTS        체크 결과
+        #                                             정답문장          TTS        체크 결과
     # return render_template('6th_test.html', target = sentence2, sound = sentence1, ck=String)
-    return jsonify({'id':stt_id})
+    return jsonify({'sound':sentence2})
 
-
-# @app.route('/stt')
-# def stt():
-#     return render_template('6th_test.html')
-
-
-
-
-# DATABASE_URI = 'ijm.db'
-# # ---- DB에서 데이터를 불러오기 ----
-# conn = sql.connect(DATABASE_URI, isolation_level=None)
-# cur = conn.cursor()
-# cur.execute(
-#     'CREATE TABLE IF NOT EXISTS STT (id TEXT, p TEXT, url TEXT)')
-# id = 1
-# p = "안녕하세요. 오늘도 멋진 하루 되세요"
-# url = 'C:/Users/admin/Desktop/SoftLand (1)/game/6_stt/sound/정답1.wav'
-
-# conn = sql.connect(DATABASE_URI, isolation_level=None)
-# cur = conn.cursor()
-# cur.execute("""INSERT INTO STT(id, p, url) 
-#                     VALUES(?, ?, ?)""", (id, p, url))
-
-# cur.execute("SELECT * FROM STT")
-# db_text = str(cur.fetchmany(size=1))
-
-# # 경로와 정답Text만 추출하기 위한 처리
-# db_List = db_text.split("'")
-
-# global sound_target
-# sound_url = db_List[5]    # 경로
-# sound_target = db_List[3] # 정답Text
-# dic = {'1' : sound_target} # 정답 Text
-
-
-# @app.route('/sound')
-# def sound():
-    
-#     return render_template('6th_test.html', target=dic['1'])
-
-# @app.route('/STT', methods=['POST', 'GET'])
-# def STT():
-    
-#     String_sound = ''  # 녹음파일 Text
-#     String_target = '' # 정답 Text
-    
-#     sleep(5)
-#     count = 1
-    
-#     #---------------------------------------------------------------------------
-#     #      STT Open API
-#     #---------------------------------------------------------------------------
-#     if request.method == 'POST':
-#         openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Recognition"
-#         accessKey = "f0f9fd15-daef-4655-b516-d7a9711c696a" 
-#         audioFilePath = "C:/Users/admin/Downloads/정답1.wav" # 다운로드한 음성파일을 여기에 넣어서 Text로 바꾸기
-            
-#         languageCode = "korean"
-        
-#         file = open(audioFilePath, "rb")
-#         audioContents = base64.b64encode(file.read()).decode("utf8")
-#         file.close()
-        
-#         requestJson = {    
-#             "argument": {
-#                 "language_code": languageCode,
-#                 "audio": audioContents
-#             }
-#         }
-        
-#         http = urllib3.PoolManager()
-#         response = http.request(
-#         "POST",
-#             openApiURL,
-#             headers={"Content-Type": "application/json; charset=UTF-8","Authorization": accessKey},
-#             body=json.dumps(requestJson)
-#         )
-        
-#         print("[responseCode] " + str(response.status))
-#         print("[responBody]")
-#         print("===== 결과 확인 ====")
-
-#         # 출력결과는 쓸때없는 내용이 들어가기 때문에 필요한 부분만 가져오기
-#         string = str(response.data,"utf-8")
-#         List = string.split('"')
-#         List = List[-2]
-#         List = List[:-1]
-#         print(List)
-#         # 녹음한 음성을 처리한 결과를 List변수에 담는다.
-        
-        
-#         # dic = {'1' : "안녕하세요. 오늘도 멋진 하루 되세요"}
-        
-        
-#         # NLP 유사도검사를 위해 정답Text와 녹음하고 Text로 바꾼 결과를 변수에 담에서 NLP모델에 넘긴다.
-#         # 녹음파일 Text
-#         String_sound = List
-        
-#         # 정답Text
-#         String_target = sound_target
-        
-#         print(List)
-        
-#         #---------------------------------------------------------------------------
-#         #       유사도 검사 NLP Open API
-#         #---------------------------------------------------------------------------
-        
-#         openApiURL = "http://aiopen.etri.re.kr:8000/ParaphraseQA"
-#         accessKey = "f0f9fd15-daef-4655-b516-d7a9711c696a"
-#         sentence1 = String_sound
-#         sentence2 = String_target
-        
-#         requestJson = {
-#         "argument": {
-#             "sentence1": sentence1,
-#             "sentence2": sentence2
-#             }
-#         }
-        
-#         http = urllib3.PoolManager()
-#         response = http.request(
-#             "POST",
-#             openApiURL,
-#             headers={"Content-Type": "application/json; charset=UTF-8","Authorization" :  accessKey},
-#             body=json.dumps(requestJson)
-#         )
-        
-#         print("[responseCode] " + str(response.status))
-#         print("[responBody]")
-#         print(str(response.data,"utf-8"))
-
-#         NLP_String = str(response.data,"utf-8")
-#         NLP_List = NLP_String.split('"')
-#         print(NLP_List)
-        
-#         NLP_reuslt = NLP_List[-2]
-#         # NLP_reuslt = NLP_target[:-1]
-#         print(NLP_reuslt)
-        
-#         #--------------------------------------------------------------------------
-#         #     검증 결과 추출 및 전송
-#         #--------------------------------------------------------------------------
-        
-#         String = ''
-#         if NLP_reuslt == 'paraphrase' :
-#             String += '유사합니다'
-#         else:
-#             String += '유사하지 않습니다'
-            
-#         os.remove(audioFilePath)
-#         #                                             정답문장          TTS        체크 결과
-#         return render_template('6th_test.html', target = sentence2, sound = sentence1, ck=String)
 
 
 ################## 결과페이지 : 대시보드 ###################
@@ -790,22 +630,6 @@ def STT():
 @app.route('/result')
 def result():
     return render_template('dashboard.html')
-
-# @app.route('/charts')
-# def charts():
-#     return render_template('charts.html')
-
-# @app.route('/sidenav')
-# def sidenav():
-#     return render_template('layout-sidenav-light.html')
-
-# @app.route('/layoutstatic')
-# def layoutstatic():
-#     return render_template('layout-static.html')
-
-# @app.route('/tables')
-# def tables():
-#     return render_template('tables.html')
 
 
 @app.route('/aboutus')
@@ -823,5 +647,5 @@ if __name__ == '__main__':
     # https://flask.palletsprojects.com/en/2.0.x/api/#flask.Flask.run
     # https://snacky.tistory.com/9
      # host주소와 port number 선언
-    app.run(host='0.0.0.0', debug=True)  
+    app.run(host='0.0.0.0')  
     
